95490ecefd2b58ec2d7810f1cadca0aceeac862e
Bethany Milford <bmilford@F2N028.42adel.org.au>
2024-03-05 16:36:50 +1030

    first push

---
+++ ft_bzero2.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmilford <bmilford@student.42adel.org.au>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/05 15:40:23 by bmilford          #+#    #+#             */
+/*   Updated: 2024/03/05 16:31:57 by bmilford         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <strings.h>
+
+void	*memset(void *b, int c)
+{
+	int	index;
+
+	index = 0;
+	while (c[index] != '\0')
+	{
+		if (c[index] >= 32 && c[index] <= 126)
+		{
+			c[index] = 48;
+			index++;
+		}
+	}
+	return (c[index]);
+}
---
+++ ft_strncmp.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmilford <bmilford@student.42adel.org.au>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/05 16:15:48 by bmilford          #+#    #+#             */
+/*   Updated: 2024/03/05 16:27:18 by bmilford         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unisgned int	index;
+
+	index = 0;
+	while (s1[index] != '\0' && s2[index] != '\0' && index < n)
+	{
+		if (s1[index] > s2[index])
+			return (1);
+		else if (s1[index] < s2[index])
+			return (-1);
+		else
+			index++;
+	}
+	while ((s1[index] == '\0' || s2[index] '\0') && index < n)
+	{
+		if (s1[index] == '\0' && s2[index] == '\0')
+			return (0);
+		else if (s1[index] < s2[index])
+			return (-1);
+		return (1);
+	}
+	return (0);
+}

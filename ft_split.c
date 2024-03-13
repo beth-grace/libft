e076300178092823474f7a4222117939b9bc62f1
Bethany Milford <bmilford@F3C025.42adel.org.au>
2024-03-13 16:59:54 +1030

    strtrim and split

---
+++ ft_split.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmilford <bmilford@student.42adel.o>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/13 14:24:03 by bmilford          #+#    #+#             */
+/*   Updated: 2024/03/13 16:45:31 by bmilford         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static char	count(char *s, c)
+{
+	int	index;
+	int	size;
+
+	index = 0;
+	size = 0;
+	while (s[index] != '\0')
+	{
+		if(s[index] == c)
+		{
+			while (s[index] == c)
+				index++;
+			size++;
+		}
+		else
+			index++;
+	}
+	return (size);
+}
+
+char	**ft_split(char const *s, char c)
+
+{
+	int		index;
+	char	**dest;
+	int		size;
+	int		string;
+
+	index = 0;
+	size = 0;
+	string = 0;
+	*dest = (char *)malloc((count(s,c) + 1) * sizeof(char));
+	while (s[index] != '\0')
+	{
+		if (s[index] != c)
+		{
+			size = 0;
+			while (s[index] != '\0' && s[index] != c)
+			{
+				index++;
+				size++;
+			}
+			dest[string] = ft_substr(s, index - size, size);
+			string++;
+		}
+		else
+			index++;
+	}
+	return (dest);
+}
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,16 +6,51 @@
 /*   By: bmilford <bmilford@student.42adel.o>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 15:59:37 by bmilford          #+#    #+#             */
-/*   Updated: 2024/03/12 16:27:03 by bmilford         ###   ########.fr       */
+/*   Updated: 2024/03/13 16:55:43 by bmilford         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *s2)
+static int	 loop(char *c,char *out)
 {
 	int	index;
+	int	test;
 
 	index = 0;
+	test = 0;
+	while (c[index] != '\0')
+	{
+		while (c[index] != out[test])
+		   index++;
+		test++;
+		index = 0;
+	}
+	return (test);
+}
 
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int		index;
+	int		out;
+	int		s;
+	char	*dest;
 
+	out = 0;
+	s = 0;
+	out  = loop(*set, *s1);
+	dest = (char *)malloc((out + 1) * sizeof(char));
+	index = 0;
+	while (s1[index] != '\0')
+	{
+		{
+			while (s1[index] != set[s])
+				s++;
+			index++;
+			s = 0;
+		}
+		dest[out++] = s1[index++];
+	}
+	dest[out] = '\0';
+	return (dest);
+}

691cb9670cf9d129a942a4a40bf7c3c4ffcff141
Beth Laptop <beefie@Beefie>
2024-03-14 00:26:43 +1030

    fixed strtrim to be better

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,51 +6,46 @@
 /*   By: bmilford <bmilford@student.42adel.o>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 15:59:37 by bmilford          #+#    #+#             */
-/*   Updated: 2024/03/13 16:55:43 by bmilford         ###   ########.fr       */
+/*   Updated: 2024/03/14 00:26:27 by beefie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	 loop(char *c,char *out)
+static int	 loop(char c,char *str)
 {
 	int	index;
-	int	test;
 
 	index = 0;
-	test = 0;
-	while (c[index] != '\0')
-	{
-		while (c[index] != out[test])
-		   index++;
-		test++;
-		index = 0;
-	}
-	return (test);
+	while (str[index] == c)
+		index++;
+	return (index);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	int		index;
 	int		out;
+	int		start;
+	int		end;
 	int		s;
 	char	*dest;
 
-	out = 0;
-	s = 0;
-	out  = loop(*set, *s1);
-	dest = (char *)malloc((out + 1) * sizeof(char));
+	start = loop(set, s1); 
+	end = ft_strlen(s1);
+	dest = (char *)malloc((end - start + 1) * sizeof(char));
 	index = 0;
+	while (s1[index] == set)
+		index++;
+	end--;
+	while (s1[end] == set)
+		end--;
 	while (s1[index] != '\0')
 	{
-		{
-			while (s1[index] != set[s])
-				s++;
-			index++;
-			s = 0;
-		}
-		dest[out++] = s1[index++];
+		dest[out] = s1[index];
+		index++;
+		out++;
 	}
-	dest[out] = '\0';
+	dest[out] == '\0';
 	return (dest);
 }

d0c11b120101f63d8a493ae4432d20456b26f9c3
Beth Laptop <beefie@Beefie>
2024-07-27 14:08:41 +0930

    fixed makefile

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bmilford <bmilford@student.42adel.org.au>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 15:06:41 by bmilford          #+#    #+#             */
-/*   Updated: 2024/07/19 18:01:24 by beefie           ###   ########.fr       */
+/*   Updated: 2024/07/27 13:57:42 by beefie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ size_t	ft_strlen(const char *s);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t len);
+vo																																										id	*ft_memmove(void *dst, const void *src, size_t len);
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
